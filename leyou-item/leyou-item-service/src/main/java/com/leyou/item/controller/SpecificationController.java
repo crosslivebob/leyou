06b04f803290943f9b5ecedc194d4378b1f21532
crosslivebob <biefaquan@gmail.com>
2021-06-14 12:46:19 +0800

    first commit

---
+++ leyou-item/leyou-item-service/src/main/java/com/leyou/item/controller/SpecificationController.java
@@ -0,0 +1,54 @@
+package com.leyou.item.controller;
+
+import com.leyou.item.pojo.SpecGroup;
+import com.leyou.item.pojo.SpecParam;
+import com.leyou.item.service.SpecificationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.CollectionUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("spec")
+public class SpecificationController {
+
+    @Autowired
+    private SpecificationService specificationService;
+
+    @GetMapping("groups/{cid}")
+    public ResponseEntity<List<SpecGroup>> queryGroupsByCid(@PathVariable("cid") Long cid) {
+        List<SpecGroup> groups = this.specificationService.queryGroupsByCid(cid);
+        if (CollectionUtils.isEmpty(groups)) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(groups);
+    }
+
+    @GetMapping("params")
+    public ResponseEntity<List<SpecParam>> queryParams(@RequestParam(name = "gid", required = false) Long gid,
+                                                       @RequestParam(name = "cid", required = false) Long cid,
+                                                       @RequestParam(value = "generic", required = false)Boolean generic,
+                                                       @RequestParam(value = "searching", required = false)Boolean searching) {
+        List<SpecParam> params = this.specificationService.queryParams(gid, cid, generic, searching);
+        if (CollectionUtils.isEmpty(params)) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(params);
+    }
+
+    @GetMapping("{cid}")
+    public ResponseEntity<List<SpecGroup>> querySpecsByCid(@PathVariable("cid") Long cid){
+        List<SpecGroup> list = this.specificationService.querySpecsByCid(cid);
+        if(list == null || list.size() == 0){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return ResponseEntity.ok(list);
+    }
+}
